bca0fc930049dc53c8e80d2f1cd8cad231d6fa39
JeromeLareau <74720136+JeromeLareau@users.noreply.github.com>
2026-04-22 15:10:13 -0400

    add cooldown and duplicate

--- scanner/processing.py
+++ scanner/processing.py
@@ -1,7 +1,11 @@
 from scanner.ocr import retry_with_adaptive_threshold, retry_with_gray_filter, scan_card_name
-from scanner.scryfall import get_card_by_fuzzy_name
+from scanner.scryfall import ScryfallEndpoint, safe_scryfall_lookup
+
+last_card_id = None
 
 def process_cards(card_queue, stop_event):
+    global last_card_id
+    
     while not stop_event.is_set():
         card_image = card_queue.get()
         if card_image is None:
@@ -10,19 +14,26 @@ def process_cards(card_queue, stop_event):
         name = scan_card_name(card_image)
         print("OCR:", name)
 
-        card = get_card_by_fuzzy_name(name)
+        card = safe_scryfall_lookup(endpoint=ScryfallEndpoint.FUZZY, name=name)
         if card is None:
             print("Retrying with grayscale filter...")
             ocr_name = retry_with_gray_filter()
-            card = get_card_by_fuzzy_name(ocr_name)
+            card = safe_scryfall_lookup(endpoint=ScryfallEndpoint.FUZZY, name=ocr_name)
             
         if card is None:
             print("Retrying with adaptive threshold...")
             ocr_name = retry_with_adaptive_threshold()
-            card = get_card_by_fuzzy_name(ocr_name)
+            card = safe_scryfall_lookup(endpoint=ScryfallEndpoint.FUZZY, name=ocr_name)
+            
         if card is None:
             print("❌ No card found on Scryfall")
-        else:
-            print("✅ FOUND:", card["name"])
+            
+        else:            
+            if card["id"] == last_card_id:
+                print("⚠️ Duplicate card detected, skipping")
+                card_queue.task_done()
+                continue
+            print("✅ Card added to collection:", card["name"])
+            last_card_id = card["id"]
 
         card_queue.task_done()
--- scanner/scryfall.py
+++ scanner/scryfall.py
@@ -1,9 +1,34 @@
+import enum
+from time import time
+
 import requests
 
 SCRYFALL_NAMED_URL = "https://api.scryfall.com/cards/named"
 
 class ScryfallError(Exception):
     pass
+
+SCRYFALL_MIN_INTERVAL = 0.6
+last_scryfall_call = 0
+
+class ScryfallEndpoint(enum.Enum):
+    FUZZY = "fuzzy"
+
+def safe_scryfall_lookup(endpoint: ScryfallEndpoint, name):
+    global last_scryfall_call
+    now = time.time()
+
+    wait = SCRYFALL_MIN_INTERVAL - (now - last_scryfall_call)
+    if wait > 0:
+        time.sleep(wait)
+
+    match endpoint:
+        case ScryfallEndpoint.FUZZY:
+            card = get_card_by_fuzzy_name(name)
+            last_scryfall_call = time.time()
+            return card
+
+    return None
 
 def get_card_by_fuzzy_name(name: str) -> dict | None:
     if not name:
